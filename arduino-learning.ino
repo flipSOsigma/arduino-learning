c6eae51ccff87bb14e38d10e724fa173b1bfcff0
alif mahendra <nacubes20@gmail.com>
2025-04-30 17:33:53 +0700

    init

---
+++ arduino-learning.ino
@@ -0,0 +1,33 @@
+int jam= 0;
+int menit= 0;
+int detik= 0;
+
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("Hello World");
+}
+
+void loop() {
+  if ((menit % 5) == 0){
+    digitalWrite(2, HIGH);
+  }else{
+    digitalWrite(2, LOW);
+  }
+  Serial.print(jam);
+  Serial.print(":");
+  Serial.print(menit);
+  Serial.print(":");
+  Serial.println(detik);
+  detik++;
+  delay(10);
+  if (detik == 60){
+    menit++;
+    detik = 0;
+  }
+  if (menit == 60){
+    jam++;
+    menit = 0;
+  }
+}
